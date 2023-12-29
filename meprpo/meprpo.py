98a02c7aef8d47d31a8e064ae79b6cbe6be5d407
Jozef-Lauko <124598366+Jozef-Lauko@users.noreply.github.com>
2023-12-29 23:01:11 +0100

    Update meprpo.py

--- meprpo/meprpo.py
+++ meprpo/meprpo.py
@@ -4,13 +4,11 @@ from .neo4j_fetch_data import fetch_nodes_and_relationships
 from .neo4j_calculate_correlations import calculate_correlations
 from .neo4j_fetch_paths import calculate_distances_and_sums
 from .neo4j_correlation_heatmaps import neo4j_correlation_heatmaps
-from .neo4j_display_edges import display_node_info_edges_counts
 
 
 __all__ = [
     "connect_to_neo4j",
     "display_node_info",
-    "display_node_info_edges_counts",
     "fetch_nodes_and_relationships",
     "calculate_correlations",
     "calculate_distances_and_sums",
